6300b47625b630b6554d878fd0f2ae409cb20ded
gongping1990 <wx_2d4ee0328b904aa789f1889f4721f6f3@git.code.tencent.com>
2019-06-11 15:51:51 +0800

    接口1

--- src/mixins/test.js
+++ src/mixins/test.js
@@ -8,6 +8,22 @@ export default class testMixin extends wepy.mixin {
     tap () {
       this.mixin = 'mixin data was changed'
       console.log('mixin method tap')
+    },
+    onChangeTabbar(event) {
+      switch (event.detail) {
+        case 0:
+          wx.redirectTo({ url: '/pages/index' });
+          break;
+        case 1:
+          wx.redirectTo({ url: '/pages/praiseOfWorks' });
+          break;
+        case 2:
+          wx.redirectTo({ url: '/pages/card' });
+          break;
+        case 3:
+          wx.redirectTo({ url: '/pages/user' });
+          break;
+      }
     }
   }
 

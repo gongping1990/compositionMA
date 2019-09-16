5e5d773fe2249f576101a40062618b2aa3de0b84
gongping1990 <wx_2d4ee0328b904aa789f1889f4721f6f3@git.code.tencent.com>
2019-09-16 18:31:58 +0800

    修改11

--- src/utils/getDateDiff.js
+++ src/utils/getDateDiff.js
@@ -1,42 +1,41 @@
+const getDateDiff = (dateTime) => {
+  let dateTimeStamp = new Date(dateTime).getTime();
+  let result = '';
+  let minute = 1000 * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+  let halfamonth = day * 15;
+  let month = day * 30;
+  let year = day * 365;
+  let now = new Date().getTime();
+  let diffValue = now - dateTimeStamp;
+  if (diffValue < 0) {
+    return;
+  }
+  let monthEnd = diffValue / month;
+  let weekEnd = diffValue / (7 * day);
+  let dayEnd = diffValue / day;
+  let hourEnd = diffValue / hour;
+  let minEnd = diffValue / minute;
+  let yearEnd = diffValue / year;
+  if (yearEnd >= 1) {
+    result = dateTime;
+  } else if (monthEnd >= 1) {
+    result = '' + parseInt(monthEnd) + '月前';
+  } else if (weekEnd >= 1) {
+    result = '' + parseInt(weekEnd) + '周前';
+  } else if (dayEnd >= 1) {
+    result = '' + parseInt(dayEnd) + '天前';
+  } else if (hourEnd >= 1) {
+    result = '' + parseInt(hourEnd) + '小时前';
+  } else if (minEnd >= 1) {
+    result = '' + parseInt(minEnd) + '分钟前';
+  } else {
+    result = '刚刚';
+  }
+  return result;
+};
 
-// export default {
-//   getDateDiff
-// }
-//
-// function getDateDiff(dateTime) {
-//   let dateTimeStamp = new Date(dateTime).getTime();
-//   let result = '';
-//   let minute = 1000 * 60;
-//   let hour = minute * 60;
-//   let day = hour * 24;
-//   let halfamonth = day * 15;
-//   let month = day * 30;
-//   let year = day * 365;
-//   let now = new Date().getTime();
-//   let diffValue = now - dateTimeStamp;
-//   if (diffValue < 0) {
-//     return;
-//   }
-//   let monthEnd = diffValue / month;
-//   let weekEnd = diffValue / (7 * day);
-//   let dayEnd = diffValue / day;
-//   let hourEnd = diffValue / hour;
-//   let minEnd = diffValue / minute;
-//   let yearEnd = diffValue / year;
-//   if (yearEnd >= 1) {
-//     result = dateTime;
-//   } else if (monthEnd >= 1) {
-//     result = "" + parseInt(monthEnd) + "月前";
-//   } else if (weekC >= 1) {
-//     result = "" + parseInt(weekEnd) + "周前";
-//   } else if (dayC >= 1) {
-//     result = "" + parseInt(dayEnd) + "天前";
-//   } else if (hourC >= 1) {
-//     result = "" + parseInt(hourEnd) + "小时前";
-//   } else if (minC >= 1) {
-//     result = "" + parseInt(minEnd) + "分钟前";
-//   } else {
-//     result = "刚刚";
-//   }
-//   return result;
-// }
+module.exports = {
+  getDateDiff: getDateDiff
+};
